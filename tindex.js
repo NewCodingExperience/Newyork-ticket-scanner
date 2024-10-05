c1278f44b8e6846a0ff391eddcb5783e69d1b3d9
NewExperience <161090277+NewCodingExperience@users.noreply.github.com>
2024-10-05 16:17:02 -0400

    Update tindex.js
    New recent final copy of tindex.js

--- tindex.js
+++ tindex.js
@@ -1,26 +1,14 @@
-// document.getElementById("count").innerText = 5
 
-// let count = 5
-// count = count + 1
-// console.log(count)
+let countEl=document.getElementById("count-el");
 
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
-
-// let bonusPoints = 50;
-// console.log(bonusPoints);
-
-// bonusPoints = bonusPoints + 50;
-// console.log(bonusPoints);
-
-// bonusPoints = bonusPoints - 75;
-// console.log(bonusPoints);
-
-// bonusPoints = bonusPoints + 45;
-// console.log(bonusPoints);
+let count=0;
 
 function increment(){
-    console.log("The button has been clicked");
+    // Get the html element count el and get it as variable as result you 
+    // do countEL variable and assign to the inner text is assigned to count. 
+    // That makes count add by plus one.
+    count=count+1;
+    countEl.innerText=count;
+
 }
 
