134c9917e4818894f21da400bb0fb1974e5a7266
NewExperience <161090277+NewCodingExperience@users.noreply.github.com>
2024-10-06 10:00:58 -0400

    Update tindex.js

--- tindex.js
+++ tindex.js
@@ -1,14 +1,1 @@
 
-let countEl=document.getElementById("count-el");
-
-let count=0;
-
-function increment(){
-    // Get the html element count el and get it as variable as result you 
-    // do countEL variable and assign to the inner text is assigned to count. 
-    // That makes count add by plus one.
-    count=count+1;
-    countEl.innerText=count;
-
-}
-
